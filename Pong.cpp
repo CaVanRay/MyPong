69cb99d3fffe88a42a02e720afbaed7ec644b061
Cavan Theiss <cavanrt@gmail.com>
2025-11-21 19:00:11 -0600

    Update Pong.cpp

--- Pong.cpp
+++ Pong.cpp
@@ -15,5 +15,41 @@ int main()
 	{
 		bool running = true;
 
+		// Continue looping and processing events until user exits
+		while (running) 
+		{
+			SDL_Event event;
+			while (SDL_PollEvent{&event))
+			{
+				if (event.type == SDL_QUIT)
+				{
+					running = false;
+				}
+				else if (event.type == SDL_KEYDOWN)
+				{
+					if (event.key.keysym.sym == SDLK_ESCAPE)
+					{
+						running = false;
+					}
+				}
+			}
+			// Clear the window to black
+			SDL_SetRenderDrawColor(renderer, 0x0, 0x0, 0x0, 0xFF);
+			SDL_RenderClear(renderer);
+
+			//
+			// Rendering will happen here
+			//
+
+			// Present the backbuffer
+			SDL_RenderPresent(renderer);
+		}
+	}
+
+	// Cleanup
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	
 	return 0;
 }
