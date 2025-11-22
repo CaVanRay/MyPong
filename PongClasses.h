6d800fc647fa1422ac918512c735c3a13e836e11
Cavan Ray Theiss <cavanrt@gmail.com>
2025-11-22 15:31:15 -0600

    finishes out ball class

--- PongClasses.h
+++ PongClasses.h
@@ -37,5 +37,24 @@ const int BALL_HEIGHT = 15;
 
 class Ball
 {
+public:
+	Ball(Vec2 position)
+		: position(position)
+	{
+		rect.x = static_cast<int>(position.x);
+		rect.y = static_cast<int>(position.y);
+		rect.w = BALL_WIDTH;
+		rect.h = BALL_HEIGHT;
+	}
 
+	void Draw(SDL_Renderer* renderer)
+	{
+		rect.x = static_cast<int>(position.x);
+		rect.y = static_cast<int>(position.y);
+
+		SDL_RenderFillRect(renderer, &rect);
+	}
+
+	Vect2 position;
+	SDL_Rect rect{};
 };
