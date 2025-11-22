1483945e74fa1c84796f23e2af846346b062d0b6
Cavan Theiss <cavanrt@gmail.com>
2025-11-21 18:52:44 -0600

    Update Pong.cpp

--- Pong.cpp
+++ Pong.cpp
@@ -3,9 +3,17 @@
 const int WINDOW_WIDTH = 1280;
 const int WINDOW_HEIGHT = 720;
 
-int main() {
-	
+int main() 
+{
+	// Initialize SDL components
+	SDL_Init(SDL_INIT_VIDEO);
 
+	SDL_Window* window = SDL_CreateWindow("Pong", 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+	// Game logic
+	{
+		bool running = true;
 
 	return 0;
 }
