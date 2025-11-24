83c1d4e895769bac70945c04bb5ee3e7ea2d04d1
Cavan Ray Theiss <cavanrt@gmail.com>
2025-11-23 19:36:58 -0600

    updates PlayerScore from SDL2 to SDL3

--- PongClasses.h
+++ PongClasses.h
@@ -90,11 +90,12 @@ class PlayerScore
 public:
 	PlayerScore(Vec2 position, SDL_Renderer* renderer, TTF_Font* font) :renderer(renderer), font(font)
 	{
-		surface = TTF_RenderText_Solid(font, "0", { 0xFF, 0xFF, 0xFF, 0xFF });
+		
+		surface = TTF_RenderText_Solid(font, "0", SDL_Color{ 255,255,255,255 });
 		texture = SDL_CreateTextureFromSurface(renderer, surface);
 
-		int width, height;
-		SDL_QueryTexture(texture, nullptr, nullptr, &width, &height);
+		float width, height;
+		SDL_GetTextureSize(texture, &width, &height);
 
 		rect.x = static_cast<int>(position.x);
 		rect.y = static_cast<int>(position.y);
@@ -104,19 +105,19 @@ public:
 
 	~PlayerScore()
 	{
-		SDL_FreeSurface(surface);
+		SDL_DestroySurface(surface);
 		SDL_DestroyTexture(texture);
 	}
 
 	void Draw()
 	{
-		SDL_RenderCopy(renderer, texture, nullptr, &rect);
+		SDL_RenderTexture(renderer, texture, nullptr, &rect);
 	}
 
 	SDL_Renderer* renderer;
 	TTF_Font* font;
 	SDL_Surface* surface{};
 	SDL_Texture* texture{};
-	SDL_Rect rect{};
+	SDL_FRect rect{};
 
 };
