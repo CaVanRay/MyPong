5966fb6963988a2f139f56eadc33effa5355a431
Cavan Ray Theiss <cavanrt@gmail.com>
2025-11-23 19:12:45 -0600

    adds code to PlayerScore class

--- PongClasses.h
+++ PongClasses.h
@@ -87,5 +87,18 @@ public:
 
 class PlayerScore
 {
+public:
+	PlayerScore(Vec2 position, SDL_Renderer* renderer, TTF_Font* font) :renderer(renderer), font(font)
+	{
+		surface = TTF_RenderText_Solid(font, "0", { 0xFF, 0xFF, 0xFF, 0xFF });
+		texture = SDL_CreateTextureFromSurface(renderer, surface);
 
+		int width, height;
+		SDL_QueryTexture(texture, nullptr, nullptr, &width, &height);
+
+		rect.x = static_cast<int>(position.x);
+		rect.y = static_cast<int>(position.y);
+		rect.w = width;
+		rect.h = height;
+	}
 };
